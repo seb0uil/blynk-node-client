3ab1ee58119d20c0eddb3060616b62871d9064f1
BETTINGER Sebastien <sebastien.bettinger@adventiel.fr>
2016-10-23 17:30:52 +0200

    Next step

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
+var tls = require('tls');
+var crypto = require('crypto');
+const EventEmitter = require('events');
+
 var options = {
-  		rejectUnauthorized: false
-	};
+	rejectUnauthorized: false
+};
 
 var socket;
 var parent = this;
-	
+var event = new EventEmitter();
+
 parent.respPromises = new Map();
-	
+parent.message = [];
+
 parent.send = function(data) {
 	var commandAndBody = data.split(" ");
 	var message = parent.createMessage(commandAndBody);
@@ -15,6 +21,7 @@ parent.send = function(data) {
 
 this.msgId = 0;
 parent.createMessage = function(commandAndBody) {
+	console.log('send ' + commandAndBody);
 	var cmdBody = null;
 	var cmdString = commandAndBody[0];
 	var cmd = getCommandByString(cmdString);
@@ -25,14 +32,15 @@ parent.createMessage = function(commandAndBody) {
 		var hPwd = crypto.createHash('sha256');
 		var salt = hUser.update(username.toLowerCase()).digest();
 		hPwd.update(pwd, "utf8");
-		hPwd.update(salt, "utf8");			
-		var finalHash = hPwd.digest('base64');			
+		hPwd.update(salt, "utf8");
+		var finalHash = hPwd.digest('base64');
 		cmdBody = username + "\0" + finalHash;
 	} else if (cmd == MsgType.CREATE_DASH || cmd == MsgType.CREATE_WIDGET) {
 		cmdBody = commandAndBody.length > 1 ? commandAndBody.slice(1).join(' ') : null;
-	} else{       
+	} else{
 		cmdBody = commandAndBody.length > 1 ? commandAndBody.slice(1).join('\0') : null;
 	}
+
 	return parent.buildBlynkMessage(cmd, parent.msgId++, cmdBody);
 }
 
@@ -58,43 +66,44 @@ parent.buildBlynkMessage = function(cmd, msgId, cmdBody) {
 }
 
 function getCommandByString(cmdString) {
-    switch (cmdString) {
-        case "ping" :
-            return MsgType.PING;
-        case "login" :
-            return MsgType.LOGIN;
+	switch (cmdString) {
+		case "ping" :
+			return MsgType.PING;
+		case "login" :
+			return MsgType.LOGIN;
 		case "getToken" :
 			return MsgType.GET_TOKEN;
-        case "createDash":
-        	return MsgType.CREATE_DASH;
-        case "deleteDash":
-        	return MsgType.DELETE_DASH;
-        case "activate":
-        	return MsgType.ACTIVATE_DASHBOARD;
-        case "createWidget":
-        	return MsgType.CREATE_WIDGET;
-        case "hardware" :
-            return MsgType.HARDWARE;
-        case "loadprofilegzipped" :
-        	return MsgType.LOAD_PROFILE_GZIPPED;
-    }
+		case "createDash":
+			return MsgType.CREATE_DASH;
+		case "deleteDash":
+			return MsgType.DELETE_DASH;
+		case "activate":
+			return MsgType.ACTIVATE_DASHBOARD;
+		case "createWidget":
+			return MsgType.CREATE_WIDGET;
+		case "hardware" :
+			return MsgType.HARDWARE;
+		case "loadprofilegzipped" :
+			return MsgType.LOAD_PROFILE_GZIPPED;
+	}
 }
 
 var MsgType = {
-    RESPONSE      		:  0,
-    LOGIN         		:  2,
+	RESPONSE      		:  0,
+	LOGIN         		:  2,
 	GET_TOKEN			:  5,
-    PING          		:  6,
-    ACTIVATE_DASHBOARD	:  7,
-    TWEET         		:  12,
-    EMAIL         		:  13,
-    NOTIFY        		:  14,
-    BRIDGE        		:  15,
-    HW_SYNC       		:  16,
-    HW_INFO       		:  17,
-    HARDWARE      		:  20,
-    LOAD_PROFILE_GZIPPED:  24,
- 	SYNC				:  25,
+	PING          		:  6,
+	ACTIVATE_DASHBOARD	:  7,
+	DEACTIVATE_DASHBOARD:  8,
+	TWEET         		:  12,
+	EMAIL         		:  13,
+	NOTIFY        		:  14,
+	BRIDGE        		:  15,
+	HW_SYNC       		:  16,
+	HW_INFO       		:  17,
+	HARDWARE      		:  20,
+	LOAD_PROFILE_GZIPPED:  24,
+	SYNC				:  25,
 	CREATE_DASH			:  21,
 	DELETE_DASH 		:  23,
 	CREATE_WIDGET		:  33
@@ -106,83 +115,161 @@ hardware = function(dashboardId, pinType, pinCommand, pinId, pinValue) {
 	if (pinValue != undefined)
 		command = command + " " + pinValue;
 	return new Promise(function (resolve, reject) {
-		if (pinCommand === 'r') {
-			resolve('done');
-		} else {
+//		if (pinCommand === 'r') {
+//			resolve('done');
+//		} else {
 			parent.respPromises.set(parent.msgId, {
 				"resolve": resolve,
 				"reject": reject
 			});
-		}
+//		}
 		parent.send(command);
 	})
 }
 
-
 //
 connect = function(login, pwd, host, port) {
+	var innerConnect = function(host, port) {
+		return new Promise(function (resolve, reject) {
+			parent.socket = tls.connect(port, host, options,resolve);
+		})
+	};
+
 	return new Promise(function (resolve, reject) {
+
 		parent.respPromises.set(parent.msgId, {
 			"resolve": resolve,
 			"reject": reject
 		});
-		parent.socket = tls.connect(port, host, options,function() {
-			parent.send('login ' + login + ' ' + pwd);
-		})
-	
+
+
+		innerConnect(host, port)
+			.then(
+				parent.send('login ' + login + ' ' + pwd)
+			);
+		//parent.socket = tls.connect(port, host, options,function() {
+		//	parent.send('login ' + login + ' ' + pwd);
+		//})
+
 		parent.socket.on('data', function(data) {
 			var msgId = data.readUInt16BE(1);
+
+			//console.log('get response for messageId ' + msgId);
 			var r =	parent.respPromises.get(msgId);
+
+			var innerResolve = function(msgType, fields, resp, result) {
+				if (typeof r !== 'undefined') {
+					r.resolve(fields);
+				}
+
+				resp.trim && (resp = resp.trim());
+				/* Emit generic message */
+				event.emit('message', result, fields, resp);
+				/* Emit message for type */
+				switch (msgType) {
+					case MsgType.SYNC:
+						event.emit('sync', result, fields, resp);
+						break;
+				}
+				/* Emit message for dashboard uddate */
+				try {
+					var dashId = resp.substr(0, 1);
+					event.emit('dash' + dashId, result, fields, resp);
+				} catch (e) {
+					event.emit('message', result, fields, resp);
+				}
+				/* Emit specific message for pin (d1, v5 etc..) */
+				try {
+					var trigger = resp.substr(2, 1) + resp.substr(5, 1);
+					event.emit(trigger, result, fields, resp);
+				} catch (e) {
+					event.emit('message', result, fields, resp);
+				}
+			};
 
 			switch (data[0]) {
 				case MsgType.RESPONSE:
 					var responseCode = data.readUInt16BE(3);
 					r.resolve(responseCode);
 					break;
+				case MsgType.SYNC:
 				case MsgType.HARDWARE:
-						var resp = data.toString('utf8', 5);
-						var fields = resp.split('\0');
-						r.resolve(fields);
+					var resp = data.toString('utf8', 5);
+					var fields = resp.split('\0');
+					var result = {
+						dashboardId : fields[0],
+						pin : fields[1].substr(0,1) + fields[2],
+						value : fields[fields.length-2]
+					};
+					innerResolve(data[0], fields, resp, result);
 					break;
 				case MsgType.GET_TOKEN:
-						var resp = data.toString('utf8', 5);
-						r.resolve(resp);
+					var resp = data.toString('utf8', 5);
+					console.log('token ' + resp);
+					r.resolve(resp);
 					break;
 				case MsgType.LOAD_PROFILE_GZIPPED:
-						var buf = new Buffer(data.length - 5);
-						data.copy(buf, 0, 5);
-						zlib.unzip(buf, (err, buffer) => {
-							  if (!err) {
-								var resp = buffer.toString('utf8');
-								r.resolve(resp);
-							  } else {
-								r.reject(err);
-							  }
-						});
+					var buf = new Buffer(data.length - 5);
+					data.copy(buf, 0, 5);
+					zlib.unzip(buf, function(err, buffer) {
+						if (typeof err === 'undefined') {
+							var resp = buffer.toString('utf8');
+							r.resolve(resp);
+						} else {
+							r.reject(err);
+						}
+					});
 					break;
-				case MsgType.SYNC:
-					r.resolve("sync");
+				case MsgType.ACTIVATE_DASHBOARD:
+				case MsgType.DEACTIVATE_DASHBOARD:
 					break;
 				default:
-					r.resolve("Response raw data: " + data);
+					innerResolve(data[0], fields, resp);
+					//r.resolve("Response raw data: " + data);
 					break;
 			}
 			parent.respPromises.delete(msgId);
 		});
 	});
-}
+};
+
 getToken = function(dashboardId) {
 	var command = "getToken " + dashboardId;
-    return new Promise(function (resolve, reject) {
+	return new Promise(function (resolve, reject) {
 		parent.respPromises.set(parent.msgId, {
 			"resolve": resolve,
 			"reject": reject
 		});
 		parent.send(command);
 	})
-}
+};
+
+activate = function(dashboardId) {
+	var command = "activate " + dashboardId;
+	return new Promise(function (resolve, reject) {
+		parent.respPromises.set(parent.msgId, {
+			"resolve": resolve,
+			"reject": reject
+		});
+		parent.send(command);
+	})
+};
+
+sync = function(dashboardId) {
+	var command = "sync"; // + dashboardId;
+	return new Promise(function (resolve, reject) {
+		parent.respPromises.set(parent.msgId, {
+			"resolve": resolve,
+			"reject": reject
+		});
+		parent.send(command);
+	})
+};
 
 exports.getToken = getToken;
 exports.connect = connect;
 exports.hardware = hardware;
+exports.activate = activate;
+exports.sync = sync;
 exports.MsgType = MsgType;
+exports.event = event;
